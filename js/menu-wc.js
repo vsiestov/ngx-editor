03cafb5d45d4b05603407d6e9f636059697955cb
Sibiraj <dev.sibiraj@outlook.com>
2018-10-10 18:54:37 +0530

    update documentation for v4.0.0

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -161,6 +161,20 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         </li>
         <li class="chapter">
             <div class="simple menu-toggler" data-toggle="collapse"
+                ${ isNormalMode ? 'data-target="#interfaces-links"' : 'data-target="#xs-interfaces-links"' }>
+                <span class="icon ion-md-information-circle-outline"></span>
+                <span>Interfaces</span>
+                <span class="icon ion-ios-arrow-down"></span>
+            </div>
+            <ul class="links collapse"
+            ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                    <li class="link">
+                        <a href="interfaces/IMaxLengthValidatorOptions.html" data-type="entity-link">IMaxLengthValidatorOptions</a>
+                    </li>
+            </ul>
+        </li>
+        <li class="chapter">
+            <div class="simple menu-toggler" data-toggle="collapse"
             ${ isNormalMode ? 'data-target="#miscellaneous-links"' : 'data-target="#xs-miscellaneous-links"' }>
                 <span class="icon ion-ios-cube"></span>
                 <span>Miscellaneous</span>
